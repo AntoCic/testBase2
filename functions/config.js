83040991ea120bca9133058a9ef73e385d100b54
AntoCic <anto.cic.127@gmail.com>
2025-05-01 13:33:54 +0200

    TRY

--- functions/config.js
+++ functions/config.js
@@ -3,9 +3,9 @@
 // ||__|||__|||__|||__|||__|||__|||__||
 // |/__\|/__\|/__\|/__\|/__\|/__\|/__\|
 
-export const onDevMod = process.env.NETLIFY_DEV === "true" || process.env.NODE_ENV === "development";
-export const APP_NAME = 'testBase2';
-export const allowedOrigins = [
-  'http://localhost',
-  'https://tempbase2.netlify.app'
-];
+// export const onDevMod = process.env.NETLIFY_DEV === "true" || process.env.NODE_ENV === "development";
+// export const APP_NAME = 'testBase2';
+// export const allowedOrigins = [
+//   'http://localhost',
+//   'https://tempbase2.netlify.app'
+// ];
