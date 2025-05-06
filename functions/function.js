56c9e37004d45b8f603f40f188d4412428468e57
AntoCic <anto.cic.127@gmail.com>
2025-05-06 18:29:56 +0200

    import FUNCTION

--- functions/function.js
+++ functions/function.js
@@ -9,6 +9,7 @@ import admin from 'firebase-admin';
 import { APP_NAME, onDevMod, allowedOrigins } from "./config";
 import { log, handlerSlackMsg } from './logger';
 import { errorsList } from './errorsList';
+import FIREBASE from './FIREBASE';
 
 const routes = {
   public: {
@@ -74,138 +75,6 @@ const routes = {
 
 
 
-// ===============================
-class FIREBASE {
-  constructor() {
-    // Fai attenzione, alcune di queste sono obbligatorie.
-    // *  FIREBASE_TYPE,
-    // *  FIREBASE_PROJECT_ID,
-    // *  FIREBASE_PRIVATE_KEY_ID,
-    // *  FIREBASE_PRIVATE_KEY,
-    // *  FIREBASE_CLIENT_EMAIL,
-    // *  FIREBASE_CLIENT_ID,
-    // *  FIREBASE_AUTH_URI,
-    // *  FIREBASE_TOKEN_URI,
-    // *  FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    // *  FIREBASE_CLIENT_X509_CERT_URL,
-    // *  FIREBASE_UNIVERSE_DOMAIN,
-    // *  FIREBASE_DATABASEURL
-    const required = {
-      type: process.env.FIREBASE_TYPE,
-      project_id: process.env.FIREBASE_PROJECT_ID,
-      private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-      private_key: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : null,
-      client_email: process.env.FIREBASE_CLIENT_EMAIL,
-      client_id: process.env.FIREBASE_CLIENT_ID,
-      auth_uri: process.env.FIREBASE_AUTH_URI,
-      token_uri: process.env.FIREBASE_TOKEN_URI,
-      auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-      client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-      universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
-      databaseURL: process.env.FIREBASE_DATABASEURL,
-
-      idIndex: Math.floor(Math.random() * 100),
-    };
-
-    const optional = {
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET ?? undefined,
-      // storageBucket: process.env.FIREBASE_STORAGE_BUCKET ?? `${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
-    };
-
-    Object.assign(this, { ...required, ...optional });
-
-    for (const key in required) {
-      if (this[key] == null) {
-        const error = `Il campo ${key} è obbligatorio e non può essere nullo o indefinito.`;
-        log.error(error)
-        throw new Error();
-      }
-    }
-
-    admin.initializeApp({
-      credential: admin.credential.cert({
-        type: this.type,
-        project_id: this.project_id,
-        private_key_id: this.private_key_id,
-        private_key: this.private_key,
-        client_email: this.client_email,
-        client_id: this.client_id,
-        auth_uri: this.auth_uri,
-        token_uri: this.token_uri,
-        auth_provider_x509_cert_url: this.auth_provider_x509_cert_url,
-        client_x509_cert_url: this.client_x509_cert_url,
-        universe_domain: this.universe_domain,
-      }),
-      databaseURL: this.databaseURL,
-      storageBucket: this.storageBucket ?? undefined
-    });
-
-    this.database = admin.database();
-    this.bucket = this.storageBucket ? admin.storage().bucket() : undefined;
-  }
-
-  // Method che risponde con un nuovo unique id ogni volta che viene chiamata
-  // newId() {
-  //   let newId = this.idIndex.toString(36)
-  //   this.idIndex++;
-  //   newId += Math.random().toString(36).substring(2, 7) // stringa casuale
-  //   newId += "-" + Date.now().toString(36) // converte in base 36
-  //   return newId;
-  // }
-
-  async get(event) {
-    try {
-      const fullPath = this.getFullPath(event)
-      const snapshot = await this.database.ref(fullPath).once('value');
-      const data = snapshot.val();
-      if (data === null) {
-        throw new Error(errorsList.notFound.key);
-      }
-      return data;
-    } catch (error) {
-      throw new Error(String(error));
-    }
-  }
-
-  async post(event) {
-    try {
-      const fullPath = this.getFullPath(event);
-      await this.database.ref(fullPath).set(event.bodyParams);
-      return;
-    } catch (error) {
-      log.error(String(error))
-      throw new Error(String(error));
-    }
-  }
-
-  async put(event) {
-    try {
-      const fullPath = this.getFullPath(event);
-      await this.database.ref(fullPath).update(event.bodyParams);
-      return;
-    } catch (error) {
-      log.error(String(error))
-      throw new Error(String(error));
-    }
-  }
-
-  async delete(event) {
-    try {
-      const fullPath = this.getFullPath(event);
-      const key = event?.queryParams?.key;
-      if (!key) { throw new Error("Try to delete, params: { key } non presente"); }
-      await this.database.ref(fullPath + '/' + key).remove();
-      return { deleted: event.pathParams.at(-1) };
-    } catch (error) {
-      log.error(String(error))
-      throw new Error(String(error));
-    }
-
-  }
-
-  getFullPath(event) { return '/' + event.pathParams.join('/'); }
-
-}
 // ATTENZIONE inizializzare FIREBASE esattamente cosi.
 let firebase = null
 try { firebase = new FIREBASE(); } catch (error) { log.error(String(error)); }
